780e19703a80a151fab34de5ff932e36558eba7d
Gio Gottardi <ManuelGGottardi@gmail.com>
2015-06-19 14:44:23 -0500

    Used the setUpTestData() method in classes that need events to eliminate repeated creation of events in the individual tests.

--- tests/tests.py
+++ tests/tests.py
@@ -32,6 +32,11 @@ def create_event(title='test', outcome='Success', name='John Doe'):
 class TestURLs(TestCase):
     """Test that each existing URL is acknowledged with an HTTP 200."""
 
+    @classmethod
+    def setUpTestData(cls):
+        """Set up the events for use in the tests in this class."""
+        cls.event = create_event()
+
     def test_index_url(self):
         """Check that the index URL receives an HTTP 200."""
         url = reverse('major-event-log:index')
@@ -39,20 +44,17 @@ class TestURLs(TestCase):
 
     def test_event_details_url(self):
         """Check that the event_details URL receives an HTTP 200."""
-        uuid = create_event().id
-        url = reverse('major-event-log:event_details', args=[uuid])
+        url = reverse('major-event-log:event_details', args=[self.event.id])
         self.assertEqual(self.client.get(url).status_code, 200)
 
     def test_event_atom_url(self):
         """Check that the event_atom URL receives an HTTP 200."""
-        uuid = create_event().id
-        url = reverse('major-event-log:event_atom', args=[uuid])
+        url = reverse('major-event-log:event_atom', args=[self.event.id])
         self.assertEqual(self.client.get(url).status_code, 200)
 
     def test_event_premis_url(self):
         """Check that the event_premis URL receives an HTTP 200."""
-        uuid = create_event().id
-        url = reverse('major-event-log:event_premis', args=[uuid])
+        url = reverse('major-event-log:event_premis', args=[self.event.id])
         self.assertEqual(self.client.get(url).status_code, 200)
 
     def test_feed_url(self):
@@ -96,6 +98,11 @@ class TestViewsCalled(TestCase):
 class TestTemplateUsed(TestCase):
     """Test that the correct template is called by the views."""
 
+    @classmethod
+    def setUpTestData(cls):
+        """Set up the events for use in the tests in this class."""
+        cls.event = create_event()
+
     def test_index_template_used(self):
         """Check that the correct template is used by the index view."""
         url = reverse('major-event-log:index')
@@ -104,22 +111,19 @@ class TestTemplateUsed(TestCase):
 
     def test_event_details_template_used(self):
         """Check that the right template is used by the event_details view."""
-        uuid = create_event().id
-        url = reverse('major-event-log:event_details', args=[uuid])
+        url = reverse('major-event-log:event_details', args=[self.event.id])
         template = 'major-event-log/event_details.html'
         self.assertTemplateUsed(self.client.get(url), template)
 
     def test_event_atom_template_used(self):
         """Check that the correct template is used by the event_atom view."""
-        uuid = create_event().id
-        url = reverse('major-event-log:event_atom', args=[uuid])
+        url = reverse('major-event-log:event_atom', args=[self.event.id])
         template = 'major-event-log/event_atom.xml'
         self.assertTemplateUsed(self.client.get(url), template)
 
     def test_event_premis_template_used(self):
         """Check that the correct template is used by the event_premis view."""
-        uuid = create_event().id
-        url = reverse('major-event-log:event_premis', args=[uuid])
+        url = reverse('major-event-log:event_premis', args=[self.event.id])
         template = 'major-event-log/event_premis.xml'
         self.assertTemplateUsed(self.client.get(url), template)
 
@@ -133,10 +137,14 @@ class TestTemplateUsed(TestCase):
 class TestGetEventOr404(TestCase):
     """Tests that the function returns the specified event or an HTTP 404."""
 
+    @classmethod
+    def setUpTestData(cls):
+        """Set up the events for use in the tests in this class."""
+        cls.event = create_event()
+
     def test_get_event_or_404_with_uuid_in_db(self):
         """Check that an event is returned when an existing UUID is given."""
-        uuid = str(create_event().id)
-        event = views.get_event_or_404(uuid)
+        event = views.get_event_or_404(str(self.event.id))
         self.assertIsInstance(event, Event)
 
     def test_get_event_or_404_with_uuid_not_in_db(self):
@@ -158,74 +166,81 @@ class TestGetEventOr404(TestCase):
 class TestModelMethods(TestCase):
     """Tests correct functionality of the 2 methods in the Event model."""
 
+    @classmethod
+    def setUpTestData(cls):
+        """Set up the events for use in the tests in this class."""
+        cls.event = create_event()
+        cls.event_failure = create_event(outcome='Failure')
+
     def test_get_absolute_url(self):
         """Check that the method returns the expected absolute URL.
 
         This method should return the URL of the event details page
         for that specific event.
         """
-        event = create_event()
-        expected = reverse("major-event-log:event_details", args=[event.id])
-        self.assertEqual(event.get_absolute_url(), expected)
+        expected = reverse("major-event-log:event_details",
+                           args=[self.event.id])
+        self.assertEqual(self.event.get_absolute_url(), expected)
 
     def test_is_success_with_success(self):
         """Check that True is returned when the outcome was a success."""
-        event = create_event()
-        self.assertEqual(event.is_success(), True)
+        self.assertEqual(self.event.is_success(), True)
 
     def test_is_success_with_failure(self):
         """Check that False is returned when the outcome was a failure."""
-        event = create_event(outcome='Failure')
-        self.assertEqual(event.is_success(), False)
+        self.assertEqual(self.event_failure.is_success(), False)
 
 
 class TestEventsUsed(TestCase):
     """Tests to make sure the correct events are being processed."""
 
+    @classmethod
+    def setUpTestData(cls):
+        """Set up the events for use in the tests in this class."""
+        cls.events = []
+        for i in range(11):
+            cls.events.append(create_event())
+
     def test_index_events(self):
         """Check that the index page shows the most recent 10 events."""
-        events = []
-        for i in range(4):
-            events.append(create_event())
         response = self.client.get(reverse('major-event-log:index'))
-        for event in events:
+        for event in self.events[1:11]:
             self.assertIn(event, response.context['events'])
 
     def test_event_details_event(self):
         """Check that the event_details page shows the correct event."""
-        event = create_event()
         response = self.client.get(reverse('major-event-log:event_details',
-                                           args=[event.id]))
+                                           args=[self.events[0].id]))
         # Check that the correct event is passed in the context.
-        self.assertEqual(response.context['event'], event)
+        self.assertEqual(response.context['event'], self.events[0])
 
     def test_event_atom_event(self):
         """Check that the event_atom page is using the correct event."""
-        event = create_event()
         response = self.client.get(reverse('major-event-log:event_atom',
-                                           args=[event.id]))
-        self.assertEqual(response.context['event'], event)
+                                           args=[self.events[0].id]))
+        self.assertEqual(response.context['event'], self.events[0])
 
     def test_event_premis_event(self):
         """Check that the event_premis page is using the correct event."""
-        event = create_event()
         response = self.client.get(reverse('major-event-log:event_premis',
-                                           args=[event.id]))
-        self.assertEqual(response.context['event'], event)
+                                           args=[self.events[0].id]))
+        self.assertEqual(response.context['event'], self.events[0])
 
     def test_feed_events(self):
         """Check that only the latest 10 events are included in the feed."""
-        events = []
-        for i in range(11):
-            events.append(create_event())
         response = self.client.get(reverse('major-event-log:feed'))
-        self.assertNotContains(response, events[0].id)
-        for event in events[1:]:
+        self.assertNotContains(response, self.events[0].id)
+        for event in self.events[1:11]:
             self.assertContains(response, event.id)
 
 
 class TestXML(TestCase):
     """Test the validity of the produced XML."""
+
+    @classmethod
+    def setUpTestData(cls):
+        """Set up the events for use in the tests in this class."""
+        cls.event = create_event()
 
     def test_event_atom_xml(self):
         """Checks for the correct structure within the Atom XML."""
@@ -237,9 +252,8 @@ class TestXML(TestCase):
             './default:author/default:name',
             './default:content'
         ]
-        event = create_event()
         response = self.client.get(reverse('major-event-log:event_atom',
-                                           args=[event.id]))
+                                           args=[self.event.id]))
         atom = ET.fromstring(response.content)
         for xpath in expected_xml_structure:
             self.assertIsNotNone(atom.find(xpath, namespace))
@@ -258,9 +272,8 @@ class TestXML(TestCase):
             './prms:eventIdentifier/prms:eventIdentifierType',
             './prms:eventDateTime'
         ]
-        event = create_event()
         response = self.client.get(reverse('major-event-log:event_premis',
-                                           args=[event.id]))
+                                           args=[self.event.id]))
         # Make sure that the PREMIS event has the expected structure.
         atom = ET.fromstring(response.content)
         for xpath in expected_premis_structure:
@@ -277,7 +290,6 @@ class TestXML(TestCase):
             './default:subtitle',
             './default:entry'
         ]
-        create_event()
         response = self.client.get(reverse('major-event-log:feed'))
         atom = ET.fromstring(response.content)
         for xpath in expected_feed_structure:
