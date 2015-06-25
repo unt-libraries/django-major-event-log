0cb671bbf5ee184d7af46f75ba3da616880014b7
Damon Kelley <damon.kelley@gmail.com>
2015-06-25 16:01:53 -0500

    Use single quotes only in add_root_elements.

--- major_event_log/feeds.py
+++ major_event_log/feeds.py
@@ -25,13 +25,13 @@ class PaginatedAtom1FeedMixin(object):
         in order to place the additional link elements into the
         desired location in the document.
         """
-        handler.addQuickElement("title", self.feed['title'])
+        handler.addQuickElement('title', self.feed['title'])
         handler.addQuickElement(
-            "link", "", {"rel": "alternate", "href": self.feed['link']})
+            'link', '', {'rel': 'alternate', 'href': self.feed['link']})
 
         if self.feed['feed_url'] is not None:
             handler.addQuickElement(
-                "link", "", {"rel": "self", "href": self.feed['feed_url']})
+                'link', '', {'rel': 'self', 'href': self.feed['feed_url']})
 
         handler.addQuickElement(
             u'link', '', self._create_link_attr(u'first', 1))
@@ -53,27 +53,27 @@ class PaginatedAtom1FeedMixin(object):
                 '',
                 self._create_link_attr(u'next_page', self.feed['next_page']))
 
-        handler.addQuickElement("id", self.feed['id'])
+        handler.addQuickElement('id', self.feed['id'])
         handler.addQuickElement(
-            "updated", rfc3339_date(self.latest_post_date()))
+            'updated', rfc3339_date(self.latest_post_date()))
 
         if self.feed['author_name'] is not None:
-            handler.startElement("author", {})
-            handler.addQuickElement("name", self.feed['author_name'])
+            handler.startElement('author', {})
+            handler.addQuickElement('name', self.feed['author_name'])
             if self.feed['author_email'] is not None:
-                handler.addQuickElement("email", self.feed['author_email'])
+                handler.addQuickElement('email', self.feed['author_email'])
             if self.feed['author_link'] is not None:
-                handler.addQuickElement("uri", self.feed['author_link'])
-            handler.endElement("author")
+                handler.addQuickElement('uri', self.feed['author_link'])
+            handler.endElement('author')
 
         if self.feed['subtitle'] is not None:
-            handler.addQuickElement("subtitle", self.feed['subtitle'])
+            handler.addQuickElement('subtitle', self.feed['subtitle'])
 
         for cat in self.feed['categories']:
-            handler.addQuickElement("category", "", {"term": cat})
+            handler.addQuickElement('category', '', {'term': cat})
 
         if self.feed['feed_copyright'] is not None:
-            handler.addQuickElement("rights", self.feed['feed_copyright'])
+            handler.addQuickElement('rights', self.feed['feed_copyright'])
 
 
 class MajorEventLogFeed(PaginatedAtom1FeedMixin, Atom1Feed):
