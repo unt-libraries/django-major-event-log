c8b9d56d3b2c28dd048953b4599df10cf8392811
Gio Gottardi <ManuelGGottardi@gmail.com>
2015-07-01 16:29:06 -0500

    Centralized the version to the __init__.py file in major_event_log. The versions used in docs/conf.py and setup.py use this version.

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 from setuptools import setup
 from codecs import open
 from os import path
+import re
 
 here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'major-event-log', '__init__.py'), 'r') as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
 
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     readme = f.read()
 
 setup(
     name='django-major-event-log',
-    version='0.1.0',
+    version=version,
     description='Django app for keeping track of major premis events.',
     long_description=readme,
     url='https://github.com/unt-libraries/django-major-event-log',
