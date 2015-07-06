9eb6f64a6f9ce523a27a6e6e58b5720f4425bc69
Gio Gottardi <ManuelGGottardi@gmail.com>
2015-07-06 14:13:41 -0500

    Added python 2, 3, and 3.4 tags in setup.py.

--- setup.py
+++ setup.py
@@ -17,6 +17,9 @@ setup(
         'Environment :: Web Environment',
         'Framework :: Django :: 1.8',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
     ]
 )
