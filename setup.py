d5a51a2aafdba5cbd61f1cc935e3080d279cba4f
gracieflores <53153376+gracieflores@users.noreply.github.com>
2022-10-31 12:26:39 -0500

    update changelog and setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-major-event-log',
-    version='1.0.0',
+    version='1.1.0',
     packages=find_packages(exclude=['tests*']),
     description='Django app for keeping track of major premis events.',
     long_description='See the home page for more information.',
